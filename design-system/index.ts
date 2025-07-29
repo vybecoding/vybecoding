494fce076508303a3cb074fd2a43758ff193d898
Happy Ducky <happy@pop-os.lan>
2025-07-29 07:36:04 -0400

    claude-04-[7/29 07:36AM]: modify {{filePath}} [SIMPLE:2 files]

---
+++ design-system/index.ts
@@ -0,0 +1,19 @@
+/**
+ * @vybecoding/design-system
+ * Core design system exports
+ */
+
+// Export tokens
+export * from './tokens/colors';
+export * from './tokens/spacing';
+export * from './tokens/typography';
+export * from './tokens/shadows';
+export * from './tokens/animations';
+
+// Export utilities
+export * from './utils/gradients';
+export * from './utils/glassmorphism';
+export * from './utils/css-modules';
+
+// Export component styles
+export * from './styles/base';
