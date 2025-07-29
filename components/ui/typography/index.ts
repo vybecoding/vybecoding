e402f526f30210a94aaf244890d8aba9d71968a4
Happy Ducky <happy@pop-os.lan>
2025-07-29 08:53:21 -0400

    claude-01-[7/29 08:53AM]: modify {{filePath}} [SIMPLE:2 files]

---
+++ components/ui/typography/index.ts
@@ -0,0 +1,10 @@
+export { Heading } from './Heading';
+export { Text } from './Text';
+export { Code } from './Code';
+export { List, ListItem } from './List';
+
+// Re-export types
+export type { HeadingProps } from './Heading';
+export type { TextProps } from './Text';
+export type { CodeProps } from './Code';
+export type { ListProps, ListItemProps } from './List';
