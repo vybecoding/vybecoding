99944577c26cf5d5e15ea2c0906db860bf5592ce
Happy Ducky <happy@pop-os.lan>
2025-07-29 16:53:09 -0400

    claude-07-[7/29 04:53PM]: modify {{filePath}} [SIMPLE:2 files]

---
+++ components/apps/AppFormSteps/index.ts
@@ -0,0 +1,5 @@
+export { BasicInfoStep } from "./BasicInfoStep";
+export { VisualAssetsStep } from "./VisualAssetsStep";
+export { LinksStep } from "./LinksStep";
+export { TechnicalStep } from "./TechnicalStep";
+export { ReviewStep } from "./ReviewStep";
