46c469b010fb69ee13bec70d7f25aeb1356bed3a
Happy Ducky <happy@pop-os.lan>
2025-07-29 09:16:57 -0400

    claude-04-[7/29 09:16AM]: modify {{filePath}} [SIMPLE:2 files]

---
+++ components/ui/layout/index.ts
@@ -0,0 +1,8 @@
+export { Container, type ContainerProps } from './Container';
+export { Section, type SectionProps } from './Section';
+export { Grid, type GridProps } from './Grid';
+export { Stack, type StackProps } from './Stack';
+export { Box, type BoxProps } from './Box';
+export { Spacer, type SpacerProps } from './Spacer';
+export { Center, type CenterProps } from './Center';
+export { Divider, type DividerProps } from './Divider';
