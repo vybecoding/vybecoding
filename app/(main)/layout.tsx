e504eb67da93a4da08b0ada566e29a2ae94bc39b
Happy Ducky <happy@pop-os.lan>
2025-07-29 18:58:43 -0400

    claude-03-[7/29 06:58PM]: modify {{filePath}} [SIMPLE:2 files]

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,16 @@
+import Navigation from '@/components/Navigation'
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <Navigation />
+      <div className="pt-[72px]">
+        {children}
+      </div>
+    </>
+  )
+}
