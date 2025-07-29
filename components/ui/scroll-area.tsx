a969f6631d744b486fcf5d301af63f31376b106b
Happy Ducky <happy@pop-os.lan>
2025-07-29 18:54:13 -0400

    claude-08-[7/29 06:54PM]: modify {{filePath}} [SIMPLE:2 files]

---
+++ components/ui/scroll-area.tsx
@@ -0,0 +1,33 @@
+import * as React from "react"
+import { cn } from "@/lib/utils"
+
+const ScrollArea = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, children, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("relative overflow-auto", className)}
+    {...props}
+  >
+    {children}
+  </div>
+))
+ScrollArea.displayName = "ScrollArea"
+
+const ScrollBar = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "absolute right-0 top-0 h-full w-2.5 bg-gray-200 dark:bg-gray-800",
+      className
+    )}
+    {...props}
+  />
+))
+ScrollBar.displayName = "ScrollBar"
+
+export { ScrollArea, ScrollBar }
