2482dee901fa1d2b1c38d1e0e5c68373a2af6449
Happy Ducky <happy@pop-os.lan>
2025-07-29 12:46:51 -0400

    claude-06-[7/29 12:46PM]: modify {{filePath}} [SIMPLE:2 files]

---
+++ components/ui/data-table/index.ts
@@ -0,0 +1,8 @@
+export { DataTable } from './DataTable';
+export type { DataTableProps } from './DataTable';
+
+export { 
+  createSortableHeader, 
+  createCheckboxColumn, 
+  createActionsColumn 
+} from './columns';
