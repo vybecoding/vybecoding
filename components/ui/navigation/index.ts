810157de5c0bd7ccdd4034b5da16c5c7ee9c0339
Happy Ducky <happy@pop-os.lan>
2025-07-29 11:22:03 -0400

    claude-04-[7/29 11:22AM]: modify {{filePath}} [SIMPLE:2 files]

---
+++ components/ui/navigation/index.ts
@@ -0,0 +1,11 @@
+export { Header } from './Header';
+export type { HeaderProps } from './Header';
+
+export { Footer } from './Footer';
+export type { FooterProps } from './Footer';
+
+export { Logo } from './Logo';
+export type { LogoProps } from './Logo';
+
+export { MobileMenu } from './MobileMenu';
+export type { MobileMenuProps, NavItem } from './MobileMenu';
